c5f2cfc6e8e5a9a11e2ec15227263f00e01d71d5
Rohit Joshi <155726606+ROHIT-J0SHI@users.noreply.github.com>
2024-03-28 16:57:16 +0530

    Add files via upload

---
+++ PlateMate/Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header' id='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Experience culinary excellence at PlateMate. Indulge in expertly crafted dishes bursting with flavor, served with a side of warm hospitality. Your next unforgettable dining adventure awaits!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
